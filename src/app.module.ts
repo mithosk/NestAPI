04e6d431f4cad0112553bf908733444424e01c2d
Luca Nicolini <mithosk85@gmail.com>
2022-02-11 23:51:50 +0100

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule]
+  imports: [
+    ProductModule,
+    TypeOrmModule.forRoot()
+  ]
 })
-export class AppModule {}
+
+export class AppModule { }
