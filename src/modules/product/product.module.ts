fd9b513599578033ebb15b650298c33ed1b987f5
Luca Nicolini <mithosk85@gmail.com>
2022-02-15 12:54:02 +0100

    Update product.module.ts

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { ProductRepository } from 'src/data/repositories/product.repository';
 
 @Module({
+  controllers: [ProductController],
   providers: [ProductService],
-  controllers: [ProductController]
+  imports: [TypeOrmModule.forFeature([ProductRepository])]
 })
+
 export class ProductModule { }
