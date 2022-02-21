114a846b5d2ad15f7bbd6ff853adc785669d014e
Luca Nicolini <luca.nicolini@flowing.it>
2022-02-21 17:51:11 +0100

    fixed module

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -3,11 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { ProductRepository } from 'src/data/repositories/product.repository';
+import { CategoryRepository } from 'src/data/repositories/category.repository';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
-  imports: [TypeOrmModule.forFeature([ProductRepository])]
+  imports: [TypeOrmModule.forFeature([
+    CategoryRepository,
+    ProductRepository
+  ])]
 })
 
 export class ProductModule { }
