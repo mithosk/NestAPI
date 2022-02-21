315f86bd0c8e56409d45c9eabc38398cfc21a1a2
Luca Nicolini <luca.nicolini@flowing.it>
2022-02-21 17:31:35 +0100

    posto for create new product

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,12 +1,17 @@
 import { ProductModel } from './product.model';
 import { ProductService } from './product.service';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 
 @Controller('products')
 export class ProductController {
     constructor(
         private readonly productService: ProductService
     ) { }
+
+    @Post()
+    async post(@Body() body: ProductModel): Promise<ProductModel> {
+        return await this.productService.create(body);
+    }
 
     @Get()
     async list(@Query() query: any): Promise<ProductModel[]> {
