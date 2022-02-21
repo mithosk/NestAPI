02709a7576d1a9f21017b278a614b98f204d77d4
Luca Nicolini <luca.nicolini@flowing.it>
2022-02-21 17:28:15 +0100

    service for create new product

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,12 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { ProductModel } from './product.model';
+import { ProductEntity } from 'src/data/entities/product.entity';
+import { CategoryEntity } from 'src/data/entities/category.entity';
 import { ProductRepository } from 'src/data/repositories/product.repository';
+import { CategoryRepository } from 'src/data/repositories/category.repository';
 
 @Injectable()
 export class ProductService {
   constructor(
+    private readonly categoryRepository: CategoryRepository,
     private readonly productRepository: ProductRepository
   ) { }
+
+  public async create(product: ProductModel): Promise<ProductModel> {
+    let categoryEntity = await this.categoryRepository.findBy(product.categoryCode)
+    if (categoryEntity == null) {
+      categoryEntity = new CategoryEntity()
+      categoryEntity.code = product.categoryCode
+      await this.categoryRepository.save(categoryEntity)
+    }
+
+    let productEntity = new ProductEntity()
+    productEntity.code = product.code
+    productEntity.description = product.description
+    productEntity.price = product.price
+    productEntity.insertDarte = new Date()
+    productEntity.category = categoryEntity
+    await this.productRepository.save(productEntity)
+
+    return {
+      code: productEntity.code,
+      categoryCode: productEntity.category.code,
+      description: productEntity.description,
+      price: productEntity.price
+    }
+  }
 
   public async list(query: any): Promise<ProductModel[]> {
     let entities = await this.productRepository.findBy({
