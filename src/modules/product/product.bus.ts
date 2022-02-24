6a595484791c3efaa3e3b2e95a4fa08cdfa1c46c
Luca Nicolini <luca.nicolini@flowing.it>
2022-02-24 02:01:35 +0100

    Create product.bus.ts

---
+++ src/modules/product/product.bus.ts
@@ -0,0 +1,15 @@
+import { Server } from 'socket.io'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class ProductBus {
+    private server: Server
+
+    public setServer(server: Server): void {
+        this.server = server
+    }
+
+    public notify(event: string, message: any): void {
+        this.server.emit(event, message)
+    }
+}
