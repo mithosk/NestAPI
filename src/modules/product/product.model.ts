ca6f472cde86fc8929aa092d56046915d62a37a6
Luca Nicolini <luca.nicolini@flowing.it>
2022-02-24 16:15:51 +0100

    Update product.model.ts

--- src/modules/product/product.model.ts
+++ src/modules/product/product.model.ts
@@ -1,6 +1,15 @@
-export interface ProductModel {
+import { IsNotEmpty } from 'class-validator'
+
+export class ProductModel {
+    @IsNotEmpty()
     code: string
+
+    @IsNotEmpty()
     categoryCode: string
+
+    @IsNotEmpty()
     description: string
+
+    @IsNotEmpty()
     price: number
 }
