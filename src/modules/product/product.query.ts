f66ef08974155447b96e9b76a664caae5560f74c
Luca Nicolini <luca.nicolini@flowing.it>
2022-03-09 17:29:30 +0100

    Update product.query.ts

--- src/modules/product/product.query.ts
+++ src/modules/product/product.query.ts
@@ -2,7 +2,9 @@ export class ProductFilter {
     text: string
 }
 
-export enum ProductSortType {
-    Code = 0,
-    Price = 1
+export enum ProductSort {
+    CodeAsc = 0,
+    CodeDesc = 1,
+    PriceAsc = 2,
+    PriceDesc = 3
 }
