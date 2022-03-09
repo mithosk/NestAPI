4a5b3601ffedf68481af17c37062b47206614126
Luca Nicolini <luca.nicolini@flowing.it>
2022-03-09 17:33:49 +0100

    Update product.repository.ts

--- src/data/repositories/product.repository.ts
+++ src/data/repositories/product.repository.ts
@@ -1,5 +1,7 @@
 import { ProductFilter } from '../filters/product.filter'
 import { ProductEntity } from '../entities/product.entity'
+import { InternalServerErrorException } from '@nestjs/common'
+import { ProductSort } from '../../modules/product/product.query'
 import { EntityRepository, Repository, SelectQueryBuilder } from 'typeorm'
 
 @EntityRepository(ProductEntity)
@@ -10,11 +12,33 @@ export class ProductRepository extends Repository<ProductEntity> {
             .getOne()
     }
 
-    public async findByFilter(filter: ProductFilter, skip?: number, take?: number): Promise<ProductEntity[]> {
+    public async findByFilter(filter: ProductFilter, sort?: ProductSort, skip?: number, take?: number): Promise<ProductEntity[]> {
         let query = this.createQueryBuilder("pro")
             .leftJoinAndSelect("pro.category", "cat")
 
         query = this.applyFilter(query, filter)
+
+        if (sort != null)
+            switch (sort) {
+                case ProductSort.CodeAsc:
+                    query = query.orderBy('pro.code', 'ASC')
+                    break
+
+                case ProductSort.CodeDesc:
+                    query = query.orderBy('pro.code', 'DESC')
+                    break
+
+                case ProductSort.PriceAsc:
+                    query = query.orderBy('pro.price', 'ASC')
+                    break
+
+                case ProductSort.PriceDesc:
+                    query = query.orderBy('pro.price', 'DESC')
+                    break
+
+                default:
+                    throw new InternalServerErrorException('unmanaged sorting')
+            }
 
         if (skip != null)
             query = query.skip(skip)
@@ -34,7 +58,7 @@ export class ProductRepository extends Repository<ProductEntity> {
     }
 
     private applyFilter(query: SelectQueryBuilder<ProductEntity>, filter: ProductFilter) {
-        if (filter.text != null) {
+        if (filter.text !== null) {
             query = query
                 .where("pro.code LIKE :text OR pro.description LIKE :text", { text: "%" + filter.text + "%" })
         }
