d3cefdb88e672930f30bd53205c460590888f85d
Luca Nicolini <luca.nicolini@flowing.it>
2022-03-17 16:27:51 +0100

    Update product.interface.ts

--- src/modules/product/product.interface.ts
+++ src/modules/product/product.interface.ts
@@ -13,3 +13,14 @@ export class ProductModel {
     @IsNotEmpty()
     price: number
 }
+
+export interface ProductQuery {
+    text: string
+}
+
+export enum ProductSortType {
+    CodeAsc = 0,
+    CodeDesc = 1,
+    PriceAsc = 2,
+    PriceDesc = 3
+}
