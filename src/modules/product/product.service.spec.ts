94694896a78956cbe7002e20b37f9bd2e4cc609e
Luca Nicolini <luca.nicolini@flowing.it>
2022-03-23 15:49:31 +0100

    Update product.service.spec.ts

--- src/modules/product/product.service.spec.ts
+++ src/modules/product/product.service.spec.ts
@@ -81,5 +81,47 @@ describe('ProductService', () => {
       expect(busMessage.price).toBe(12.45)
     })
 
+    it('create a new product with an old category', async () => {
+      let busEvent: string
+      let busMessage: any
+      jest.spyOn(bus, 'notify').mockImplementation((event, message) => {
+        busEvent = event
+        busMessage = message
+      })
+
+      await getRepository(CategoryEntity).insert({
+        code: 'CATEGORY_CODE'
+      })
+
+      let productModel = await service.create({
+        code: 'PRODUCT_CODE',
+        categoryCode: 'CATEGORY_CODE',
+        description: 'product description',
+        price: 5
+      })
+
+      let productEntity: ProductEntity = await getRepository(ProductEntity)
+        .createQueryBuilder('pro')
+        .where('pro.code=:code', { code: 'PRODUCT_CODE' })
+        .leftJoinAndSelect('pro.category', 'cat')
+        .getOne()
+
+      expect(productModel.code).toBe('PRODUCT_CODE')
+      expect(productModel.categoryCode).toBe('CATEGORY_CODE')
+      expect(productModel.description).toBe('product description')
+      expect(productModel.price).toBe(5)
+
+      expect(productEntity.code).toBe('PRODUCT_CODE')
+      expect(productEntity.category.code).toBe('CATEGORY_CODE')
+      expect(productEntity.description).toBe('product description')
+      expect(productEntity.price).toBe(5)
+
+      expect(busEvent).toBe('ProductCreated')
+      expect(busMessage.code).toBe('PRODUCT_CODE')
+      expect(busMessage.categoryCode).toBe('CATEGORY_CODE')
+      expect(busMessage.description).toBe('product description')
+      expect(busMessage.price).toBe(5)
+    })
+
   })
 })
