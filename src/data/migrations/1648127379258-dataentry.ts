7ee812f8f48619104e84606c366460fbfa641b7a
Luca Nicolini <luca.nicolini@flowing.it>
2022-03-24 14:21:29 +0100

    Update 1648127379258-dataentry.ts

--- src/data/migrations/1648127379258-dataentry.ts
+++ src/data/migrations/1648127379258-dataentry.ts
@@ -2,8 +2,8 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class dataentry1648127379258 implements MigrationInterface {
     public async up(qr: QueryRunner): Promise<void> {
-        await qr.query('INSERT INTO categories (code) VALUES (\'BOOK\')')
-        await qr.query('INSERT INTO categories (code) VALUES (\'CALENDAR\')')
+        await qr.query('INSERT INTO categories (uuid, code) VALUES (\'3260f259-a60a-4bd2-886d-adaea7916017\', \'BOOK\')')
+        await qr.query('INSERT INTO categories (uuid, code) VALUES (\'4d0109bc-b5ff-4d4d-b1a9-83cc939e09b7\', \'CALENDAR\')')
 
         await qr.query('INSERT INTO products (uuid, code, description, price, insertDarte, categoryId) VALUES (\'dbfa8f91-0f37-4c7b-b00a-7ad006d0ef33\', \'ATTIMI_20X30\', \'Book Attimi 20x30\', 14.99, \'2016-07-01\', (SELECT id FROM categories WHERE code=\'BOOK\'))')
         await qr.query('INSERT INTO products (uuid, code, description, price, insertDarte, categoryId) VALUES (\'8838f508-04c5-4ce6-857a-13549be1578b\', \'EVENTI_30X30\', \'Book Eventi 20x30\', 25.99, \'2018-12-14\', (SELECT id FROM categories WHERE code=\'BOOK\'))')
