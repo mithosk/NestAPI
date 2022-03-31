5cf22f5a4a578188ce6af832cba736ed237358e6
Luca Nicolini <luca.nicolini@flowing.it>
2022-03-31 21:32:51 +0200

    Update product.controller.spec.ts

--- src/modules/product/product.controller.spec.ts
+++ src/modules/product/product.controller.spec.ts
@@ -1,18 +1,53 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProductController } from './product.controller';
+import { getConnection } from 'typeorm'
+import { ProductBus } from './product.bus'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProductService } from './product.service'
+import { Test, TestingModule } from '@nestjs/testing'
+import { ProductController } from './product.controller'
+import { ProductEntity } from '../../data/entities/product.entity'
+import { CategoryEntity } from '../../data/entities/category.entity'
+import { ProductRepository } from '../../data/repositories/product.repository'
+import { CategoryRepository } from '../../data/repositories/category.repository'
 
 describe('ProductController', () => {
-  let controller: ProductController;
+  let controller: ProductController
+  let service: ProductService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-    }).compile();
+      providers: [ProductService, ProductBus],
+      imports: [
+        TypeOrmModule.forFeature([
+          CategoryRepository,
+          ProductRepository
+        ]),
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: 'sqlite/a1aaf182-2222-419c-b132-bcad8bd043eb',
+          entities: [
+            CategoryEntity,
+            ProductEntity
+          ],
+          synchronize: true,
+          dropSchema: true
+        })
+      ]
+    }).compile()
 
-    controller = module.get<ProductController>(ProductController);
-  });
+    controller = module.get<ProductController>(ProductController)
+    service = module.get<ProductService>(ProductService)
+  })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+  afterEach(async () => {
+    await getConnection().close()
+  })
+
+  describe('post', () => {
+
+    it('xxxxx xxxxx xxxxx', async () => {
+      expect(controller).toBeDefined()
+    })
+
+  })
+})
