da4f62fe79494123701d9c4af5fb39237df25b81
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-01 16:28:25 +0200

    Update product.controller.spec.ts

--- src/modules/product/product.controller.spec.ts
+++ src/modules/product/product.controller.spec.ts
@@ -1,6 +1,8 @@
 import { getConnection } from 'typeorm'
 import { ProductBus } from './product.bus'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { Response as HttpResponse } from 'express'
+import { ProductModel } from './product.interface'
 import { ProductService } from './product.service'
 import { Test, TestingModule } from '@nestjs/testing'
 import { ProductController } from './product.controller'
@@ -66,6 +68,58 @@ describe('ProductController', () => {
 
       expect(product.code).toEqual('PRODUCT_CODE_1')
       expect(product.categoryCode).toEqual('CATEGORY_CODE_1')
+    })
+
+  })
+
+  describe('list', () => {
+
+    it('returns a body with two products', async () => {
+      //service mock
+      jest.spyOn(service, 'list').mockImplementation(() => Promise.resolve({
+        products: [
+          {
+            id: '4720f327-23e5-46ee-a226-51c3c0453559',
+            code: 'PRODUCT_CODE_1',
+            description: 'product description',
+            categoryId: 'c03d23ec-a17d-4fe3-afb6-8aa66d56f9dd',
+            categoryCode: 'CATEGORY_CODE_1',
+            price: 5
+          },
+          {
+            id: '410edf54-8282-44f6-924c-d6784fe5e287',
+            code: 'PRODUCT_CODE_1',
+            description: 'product description',
+            categoryId: '93072e8c-0c73-4afd-89b1-431cf20f0ef0',
+            categoryCode: 'CATEGORY_CODE_1',
+            price: 5
+          }
+        ],
+        pageCount: 5,
+        productCount: 15
+      }))
+
+      //response mock
+      let responseBody: ProductModel[] = []
+
+      let httpResponse = <HttpResponse<any, Record<string, any>>>{
+
+        set(key: string, value: string) {
+          return this
+        },
+
+        json(body: ProductModel[]) {
+          responseBody = body
+        }
+      }
+
+      //execution of the function to be tested
+      await controller.list({ text: undefined }, {}, httpResponse)
+
+      //check
+      expect(responseBody.length).toEqual(2)
+      expect(responseBody[0].code).toEqual('PRODUCT_CODE_1')
+      expect(responseBody[1].code).toEqual('PRODUCT_CODE_2')
     })
 
   })
