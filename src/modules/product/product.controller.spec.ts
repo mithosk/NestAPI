ab6188180c58229c5d3fe00695583a62c7fcf972
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-04 11:27:07 +0200

    Update product.controller.spec.ts

--- src/modules/product/product.controller.spec.ts
+++ src/modules/product/product.controller.spec.ts
@@ -51,7 +51,7 @@ describe('ProductController', () => {
       jest.spyOn(service, 'create').mockImplementation(() => Promise.resolve({
         id: '9671b89b-cb60-4832-991e-ab2ccb16c79b',
         code: 'PRODUCT_CODE_1',
-        description: 'product description',
+        description: 'product description 1',
         categoryId: 'e746b4da-9a1e-423b-87a4-53fa543e8f09',
         categoryCode: 'CATEGORY_CODE_1',
         price: 5
@@ -60,7 +60,7 @@ describe('ProductController', () => {
       let product = await controller.post({
         id: '5f4f5f67-cabd-4d41-93a1-4e9a756dde1c',
         code: 'PRODUCT_CODE_2',
-        description: 'product description',
+        description: 'product description 2',
         categoryId: 'e41fc545-3421-47f2-ad81-92c1381456e1',
         categoryCode: 'CATEGORY_CODE_2',
         price: 5
@@ -81,7 +81,7 @@ describe('ProductController', () => {
           {
             id: '4720f327-23e5-46ee-a226-51c3c0453559',
             code: 'PRODUCT_CODE_1',
-            description: 'product description',
+            description: 'product description 1',
             categoryId: 'c03d23ec-a17d-4fe3-afb6-8aa66d56f9dd',
             categoryCode: 'CATEGORY_CODE_1',
             price: 5
@@ -89,7 +89,7 @@ describe('ProductController', () => {
           {
             id: '410edf54-8282-44f6-924c-d6784fe5e287',
             code: 'PRODUCT_CODE_2',
-            description: 'product description',
+            description: 'product description 2',
             categoryId: '93072e8c-0c73-4afd-89b1-431cf20f0ef0',
             categoryCode: 'CATEGORY_CODE_2',
             price: 5
@@ -100,18 +100,14 @@ describe('ProductController', () => {
       }))
 
       //response mock
-      let responseBody: ProductModel[] = []
-
+      let responseBody: ProductModel[]
       let httpResponse = <HttpResponse<any, Record<string, any>>>{
-
         set(key: string, value: string) {
           return this
         },
-
         json(body: ProductModel[]) {
           responseBody = body
         }
-
       }
 
       //execution of the function to be tested
@@ -121,6 +117,45 @@ describe('ProductController', () => {
       expect(responseBody.length).toEqual(2)
       expect(responseBody[0].code).toEqual('PRODUCT_CODE_1')
       expect(responseBody[1].code).toEqual('PRODUCT_CODE_2')
+    })
+
+    it('returns a header with pagination parameters', async () => {
+      //service mock
+      jest.spyOn(service, 'list').mockImplementation(() => Promise.resolve({
+        products: [],
+        pageCount: 3,
+        productCount: 15
+      }))
+
+      //response mock
+      let responseHeader: { [id: string]: string } = {}
+      let httpResponse = <HttpResponse<any, Record<string, any>>>{
+        set(key: string, value: string) {
+          responseHeader[key] = value
+          return this
+        },
+        json(body: ProductModel[]) { }
+      }
+
+      //execution of the function to be tested
+      await controller.list(
+        {
+          text: undefined
+        },
+        {
+          'sorttype': 'PriceAsc',
+          'pageindex': '2',
+          'pageSize': '30'
+        },
+        httpResponse
+      )
+
+      //check
+      expect(responseHeader['SortType']).toEqual('PriceAsc')
+      expect(responseHeader['PageIndex']).toEqual('2')
+      expect(responseHeader['PageSize']).toEqual('30')
+      expect(responseHeader['PageCount']).toEqual('3')
+      expect(responseHeader['ItemCount']).toEqual('15')
     })
 
   })
