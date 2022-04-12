4d5f04676383fc546eed6fc423b5187fb93b7ac2
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-12 16:24:42 +0200

    Update category.repository.ts

--- src/data/repositories/category.repository.ts
+++ src/data/repositories/category.repository.ts
@@ -3,6 +3,12 @@ import { CategoryEntity } from '../entities/category.entity'
 
 @EntityRepository(CategoryEntity)
 export class CategoryRepository extends Repository<CategoryEntity>{
+    public async findByUuid(uuid: string): Promise<CategoryEntity> {
+        return await this.createQueryBuilder('cat')
+            .where('cat.uuid=:uuid', { uuid: uuid })
+            .getOne()
+    }
+
     public async findByCode(code: string): Promise<CategoryEntity> {
         return await this.createQueryBuilder('cat')
             .where('cat.code=:code', { code: code })
