e3ae278d563d74a4a7a4f17d7439d8a3ef580f65
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-28 18:26:47 +0200

    Create auth.service.ts

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid'
+import { JwtService } from '@nestjs/jwt'
+import { sha512 } from 'sha512-crypt-ts'
+import { LoginRequest, LoginResponse } from './auth.interface'
+import { ForbiddenException, Injectable } from '@nestjs/common'
+import { UserRepository } from 'src/data/repositories/user.repository'
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly jwtService: JwtService
+    ) { }
+
+    public async login(credentials: LoginRequest): Promise<LoginResponse> {
+        let user = await this.userRepository.findByEmail(credentials.email)
+        if (user === undefined)
+            throw new ForbiddenException('user not found')
+
+        if (user.passwordHash !== sha512.hex(credentials.password))
+            throw new ForbiddenException('wrong password')
+
+        if (user.accessKey === null) {
+            user.accessKey = uuidv4()
+            await this.userRepository.save(user)
+        }
+
+        return {
+            token: this.jwtService.sign({ userId: user.uuid }),
+            refreshToken: user.accessKey,
+            userId: user.uuid
+        }
+    }
+}
