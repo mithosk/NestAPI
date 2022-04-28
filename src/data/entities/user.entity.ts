71de716c9ebf277b2ab5c22153fc417c7f439aba
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-28 12:49:11 +0200

    Create user.entity.ts

---
+++ src/data/entities/user.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryGeneratedColumn, Generated } from 'typeorm'
+
+
+@Entity('users')
+export class UserEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+
+    @Generated('uuid')
+    @Column({ unique: true })
+    uuid: string
+
+    @Column({ unique: true })
+    email: string
+
+    @Column()
+    passwordHash: string
+
+    @Column()
+    accessKey: string
+
+    @Column()
+    name: string
+
+    @Column()
+    surname: string
+
+    @Column()
+    registrationDate: Date
+
+}
