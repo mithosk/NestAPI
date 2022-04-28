60d458586da6e00cc5fe893a48ee60af82a084e0
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-28 17:47:36 +0200

    Create 1651160773859-init_database.ts

---
+++ src/data/migrations/1651160773859-init_database.ts
@@ -0,0 +1,26 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class initDatabase1651160773859 implements MigrationInterface {
+    name = 'initDatabase1651160773859'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`products\` (\`id\` int NOT NULL AUTO_INCREMENT, \`uuid\` varchar(36) NOT NULL, \`code\` varchar(255) NOT NULL, \`description\` varchar(255) NOT NULL, \`price\` float NOT NULL, \`insertDarte\` datetime NOT NULL, \`categoryId\` int NULL, UNIQUE INDEX \`IDX_98086f14e190574534d5129cd7\` (\`uuid\`), UNIQUE INDEX \`IDX_7cfc24d6c24f0ec91294003d6b\` (\`code\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`categories\` (\`id\` int NOT NULL AUTO_INCREMENT, \`uuid\` varchar(36) NOT NULL, \`code\` varchar(255) NOT NULL, \`description\` varchar(255) NULL, UNIQUE INDEX \`IDX_a4b5917e7297f757879582e145\` (\`uuid\`), UNIQUE INDEX \`IDX_77d7eff8a7aaa05457a12b8007\` (\`code\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`uuid\` varchar(36) NOT NULL, \`email\` varchar(255) NOT NULL, \`passwordHash\` varchar(255) NOT NULL, \`accessKey\` varchar(255) NULL, \`name\` varchar(255) NOT NULL, \`surname\` varchar(255) NOT NULL, \`registrationDate\` datetime NOT NULL, UNIQUE INDEX \`IDX_951b8f1dfc94ac1d0301a14b7e\` (\`uuid\`), UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`products\` ADD CONSTRAINT \`FK_ff56834e735fa78a15d0cf21926\` FOREIGN KEY (\`categoryId\`) REFERENCES \`categories\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`products\` DROP FOREIGN KEY \`FK_ff56834e735fa78a15d0cf21926\``);
+        await queryRunner.query(`DROP INDEX \`IDX_97672ac88f789774dd47f7c8be\` ON \`users\``);
+        await queryRunner.query(`DROP INDEX \`IDX_951b8f1dfc94ac1d0301a14b7e\` ON \`users\``);
+        await queryRunner.query(`DROP TABLE \`users\``);
+        await queryRunner.query(`DROP INDEX \`IDX_77d7eff8a7aaa05457a12b8007\` ON \`categories\``);
+        await queryRunner.query(`DROP INDEX \`IDX_a4b5917e7297f757879582e145\` ON \`categories\``);
+        await queryRunner.query(`DROP TABLE \`categories\``);
+        await queryRunner.query(`DROP INDEX \`IDX_7cfc24d6c24f0ec91294003d6b\` ON \`products\``);
+        await queryRunner.query(`DROP INDEX \`IDX_98086f14e190574534d5129cd7\` ON \`products\``);
+        await queryRunner.query(`DROP TABLE \`products\``);
+    }
+
+}
