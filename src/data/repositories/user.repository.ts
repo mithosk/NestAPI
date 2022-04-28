5bb76fdf00c04b94320d6ba8311b6ba3ad7ded89
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-28 12:53:29 +0200

    Create user.repository.ts

---
+++ src/data/repositories/user.repository.ts
@@ -0,0 +1,11 @@
+import { UserEntity } from '../entities/user.entity'
+import { EntityRepository, Repository } from 'typeorm'
+
+@EntityRepository(UserEntity)
+export class UserRepository extends Repository<UserEntity>{
+    public async findByEmail(email: string): Promise<UserEntity> {
+        return await this.createQueryBuilder('use')
+            .where('use.email=:email', { email: email })
+            .getOne()
+    }
+}
