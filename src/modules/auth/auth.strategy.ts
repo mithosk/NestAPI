2a46c372c38d7c98f82ccda562242358309deb1d
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-29 18:59:19 +0200

    Create auth.strategy.ts

---
+++ src/modules/auth/auth.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PassportStrategy } from '@nestjs/passport'
+
+@Injectable()
+export class AuthStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(
+        private readonly configService: ConfigService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.get<string>('JWT_KEY'),
+            ignoreExpiration: false
+        })
+    }
+
+    public async validate(payload: any) {
+        return { id: payload.userId }
+    }
+}
