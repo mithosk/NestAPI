f215f41afe9f60d99c84f9acfe1b3cef8d5d5b27
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-29 12:19:26 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from './modules/auth/auth.module'
 import { ProductModule } from './modules/product/product.module'
 import { CategoryModule } from './modules/category/category.module'
 
 @Module({
   imports: [
+    AuthModule,
     CategoryModule,
     ProductModule,
     TypeOrmModule.forRoot()
