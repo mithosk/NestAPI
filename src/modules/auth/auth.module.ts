ac5e592d67a7aa484f4ee71723909f9a3b5e019e
Luca Nicolini <luca.nicolini@flowing.it>
2022-04-29 19:06:32 +0200

    Update auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { JwtModule } from '@nestjs/jwt'
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { AuthStrategy } from './auth.strategy'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthController } from './auth.controller'
 import { ConfigModule, ConfigService } from '@nestjs/config'
@@ -8,20 +9,18 @@ import { UserRepository } from 'src/data/repositories/user.repository'
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, AuthStrategy],
     imports: [
         ConfigModule.forRoot(),
         JwtModule.registerAsync({
             imports: [ConfigModule],
+            inject: [ConfigService],
             useFactory: (cso: ConfigService) => {
                 return {
                     secret: cso.get<string>('JWT_KEY'),
-                    signOptions: {
-                        expiresIn: 28800
-                    }
+                    signOptions: { expiresIn: 28800 }
                 }
-            },
-            inject: [ConfigService]
+            }
         }),
         TypeOrmModule.forFeature([
             UserRepository
