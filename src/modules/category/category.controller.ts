d07034f081e2a86c2e663a6e0a35e51d4a60b4c5
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-24 16:20:39 +0200

    Update category.controller.ts

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -11,20 +11,26 @@ export class CategoryController {
 
     @Patch(':id')
     @UseGuards(AuthGuard('jwt'))
-    public async patch(@Param('id') id: string, @Body() body: any): Promise<CategoryModel> {
+    public async patch(@Param('id') id: string, @Body() body: Partial<CategoryModel>): Promise<CategoryModel> {
         let category = await this.service.read(id)
 
         //code
-        if (body.code !== undefined)
+        if (body.code !== undefined) {
             if (body.code === null)
                 throw new ForbiddenException('code cannot be null')
+            else if (typeof body.code !== 'string')
+                throw new ForbiddenException('it must be a string')
             else
-                category.code = body.code.toString()
-
+                category.code = body.code
+        }
 
         //description
-        if (body.description !== undefined)
-            category.description = body.description === null ? null : body.description.toString()
+        if (body.description !== undefined) {
+            if (body.description !== null && typeof body.description !== 'string')
+                throw new ForbiddenException('it must be a string')
+            else
+                category.description = body.description
+        }
 
         return await this.service.update(category)
     }
