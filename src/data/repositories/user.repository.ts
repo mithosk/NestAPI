61caf48c9bc12747a29a2ea8c73c4e2870ec8c63
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:02:37 +0200

    Update user.repository.ts

--- src/data/repositories/user.repository.ts
+++ src/data/repositories/user.repository.ts
@@ -2,16 +2,12 @@ import { UserEntity } from '../entities/user.entity'
 import { EntityRepository, Repository } from 'typeorm'
 
 @EntityRepository(UserEntity)
-export class UserRepository extends Repository<UserEntity>{
-    public async findByUuid(uuid: string): Promise<UserEntity> {
-        return await this.createQueryBuilder('use')
-            .where('use.uuid=:uuid', { uuid: uuid })
-            .getOne()
-    }
+export class UserRepository extends Repository<UserEntity> {
+	public async findByUuid(uuid: string): Promise<UserEntity> {
+		return await this.createQueryBuilder('use').where('use.uuid=:uuid', { uuid: uuid }).getOne()
+	}
 
-    public async findByEmail(email: string): Promise<UserEntity> {
-        return await this.createQueryBuilder('use')
-            .where('use.email=:email', { email: email })
-            .getOne()
-    }
+	public async findByEmail(email: string): Promise<UserEntity> {
+		return await this.createQueryBuilder('use').where('use.email=:email', { email: email }).getOne()
+	}
 }
