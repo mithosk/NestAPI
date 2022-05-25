0e7becb23ee20ed93e450e6a8549e927d0e8596b
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:05:33 +0200

    Update auth.service.ts

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,43 +6,37 @@ import { UserRepository } from '../../data/repositories/user.repository'
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly userRepository: UserRepository
-    ) { }
+	constructor(private readonly userRepository: UserRepository) {}
 
-    public async validateCredentials(credentials: LoginRequest): Promise<LoginResponse> {
-        let user = await this.userRepository.findByEmail(credentials.email)
-        if (user === undefined)
-            throw new ForbiddenException('user not found')
+	public async validateCredentials(credentials: LoginRequest): Promise<LoginResponse> {
+		let user = await this.userRepository.findByEmail(credentials.email)
+		if (user === undefined) throw new ForbiddenException('user not found')
 
-        if (user.passwordHash !== sha512.hex(credentials.password))
-            throw new ForbiddenException('wrong password')
+		if (user.passwordHash !== sha512.hex(credentials.password)) throw new ForbiddenException('wrong password')
 
-        if (user.accessKey === null) {
-            user.accessKey = uuidv4()
-            await this.userRepository.save(user)
-        }
+		if (user.accessKey === null) {
+			user.accessKey = uuidv4()
+			await this.userRepository.save(user)
+		}
 
-        return {
-            token: undefined,
-            refreshToken: user.accessKey,
-            userId: user.uuid
-        }
-    }
+		return {
+			token: undefined,
+			refreshToken: user.accessKey,
+			userId: user.uuid
+		}
+	}
 
-    public async validateAccessKey(accessKey: string, userId: string): Promise<boolean> {
-        let user = await this.userRepository.findByUuid(userId)
+	public async validateAccessKey(accessKey: string, userId: string): Promise<boolean> {
+		let user = await this.userRepository.findByUuid(userId)
 
-        return user !== undefined &&
-            user.accessKey === accessKey
-    }
+		return user !== undefined && user.accessKey === accessKey
+	}
 
-    public async resetAccessKey(userId: string): Promise<void> {
-        let user = await this.userRepository.findByUuid(userId)
-        if (user === undefined)
-            throw new ForbiddenException('user not found')
+	public async resetAccessKey(userId: string): Promise<void> {
+		let user = await this.userRepository.findByUuid(userId)
+		if (user === undefined) throw new ForbiddenException('user not found')
 
-        user.accessKey = null
-        await this.userRepository.save(user)
-    }
+		user.accessKey = null
+		await this.userRepository.save(user)
+	}
 }
