71d6508c53726ea4c4e94c465afb2a82d95cd28d
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:04:11 +0200

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,45 +2,48 @@ import { JwtService } from '@nestjs/jwt'
 import { AuthService } from './auth.service'
 import { AuthGuard } from '@nestjs/passport'
 import { Body, Controller, ForbiddenException, Post, UseGuards, Request, HttpCode } from '@nestjs/common'
-import { LoginRequest, LoginResponse, RefreshRequest, RefreshResponse, LogoutRequest, LogoutResponse } from './auth.interface'
+import {
+	LoginRequest,
+	LoginResponse,
+	RefreshRequest,
+	RefreshResponse,
+	LogoutRequest,
+	LogoutResponse
+} from './auth.interface'
 
 @Controller('rpc/auth')
 export class AuthController {
-    constructor(
-        private readonly service: AuthService,
-        private readonly jwtService: JwtService
-    ) { }
+	constructor(private readonly service: AuthService, private readonly jwtService: JwtService) {}
 
-    @Post('login')
-    @HttpCode(200)
-    public async login(@Body() body: LoginRequest): Promise<LoginResponse> {
-        let response = await this.service.validateCredentials(body)
+	@Post('login')
+	@HttpCode(200)
+	public async login(@Body() body: LoginRequest): Promise<LoginResponse> {
+		let response = await this.service.validateCredentials(body)
 
-        response.token = this.jwtService.sign({ userId: response.userId })
+		response.token = this.jwtService.sign({ userId: response.userId })
 
-        return response
-    }
+		return response
+	}
 
-    @HttpCode(200)
-    @Post('refresh')
-    public async refresh(@Body() body: RefreshRequest): Promise<RefreshResponse> {
-        if (await this.service.validateAccessKey(body.refreshToken, body.userId) === false)
-            throw new ForbiddenException('a token could not be generated')
+	@HttpCode(200)
+	@Post('refresh')
+	public async refresh(@Body() body: RefreshRequest): Promise<RefreshResponse> {
+		if ((await this.service.validateAccessKey(body.refreshToken, body.userId)) === false)
+			throw new ForbiddenException('a token could not be generated')
 
-        return {
-            token: this.jwtService.sign({ userId: body.userId })
-        }
-    }
+		return {
+			token: this.jwtService.sign({ userId: body.userId })
+		}
+	}
 
-    @HttpCode(200)
-    @Post('logout')
-    @UseGuards(AuthGuard('jwt'))
-    public async logout(@Body() body: LogoutRequest, @Request() request: any): Promise<LogoutResponse> {
-        if (body.userId !== request.user.id)
-            throw new ForbiddenException('another user cannot be logged out')
+	@HttpCode(200)
+	@Post('logout')
+	@UseGuards(AuthGuard('jwt'))
+	public async logout(@Body() body: LogoutRequest, @Request() request: any): Promise<LogoutResponse> {
+		if (body.userId !== request.user.id) throw new ForbiddenException('another user cannot be logged out')
 
-        await this.service.resetAccessKey(body.userId)
+		await this.service.resetAccessKey(body.userId)
 
-        return {}
-    }
+		return {}
+	}
 }
