94c4b03dca60e66bd0a164fbdcd9241097efa612
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:04:33 +0200

    Update auth.interface.ts

--- src/modules/auth/auth.interface.ts
+++ src/modules/auth/auth.interface.ts
@@ -1,39 +1,39 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class LoginRequest {
-    @IsEmail()
-    @IsNotEmpty()
-    email: string
+	@IsEmail()
+	@IsNotEmpty()
+	email: string
 
-    @IsString()
-    @IsNotEmpty()
-    password: string
+	@IsString()
+	@IsNotEmpty()
+	password: string
 }
 
 export interface LoginResponse {
-    token: string
-    refreshToken: string
-    userId: string
+	token: string
+	refreshToken: string
+	userId: string
 }
 
 export class RefreshRequest {
-    @IsString()
-    @IsNotEmpty()
-    userId: string
+	@IsString()
+	@IsNotEmpty()
+	userId: string
 
-    @IsString()
-    @IsNotEmpty()
-    refreshToken: string
+	@IsString()
+	@IsNotEmpty()
+	refreshToken: string
 }
 
 export class RefreshResponse {
-    token: string
+	token: string
 }
 
 export class LogoutRequest {
-    @IsString()
-    @IsNotEmpty()
-    userId: string
+	@IsString()
+	@IsNotEmpty()
+	userId: string
 }
 
-export class LogoutResponse { }
+export class LogoutResponse {}
