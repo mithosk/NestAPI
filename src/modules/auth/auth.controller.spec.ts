a72906d132f2c7895fe23b8730a1a9aad7d2f166
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:03:49 +0200

    Update auth.controller.spec.ts

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -11,121 +11,116 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { UserRepository } from '../../data/repositories/user.repository'
 
 describe('AuthController', () => {
-  let controller: AuthController
-  let service: AuthService
+	let controller: AuthController
+	let service: AuthService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-      imports: [
-        TypeOrmModule.forFeature([
-          UserRepository
-        ]),
-        TypeOrmModule.forRoot({
-          type: 'sqlite',
-          database: 'sqlite/1dcc22fc-6dd8-41b8-88a0-39a119c129bf',
-          entities: [
-            UserEntity
-          ],
-          synchronize: true,
-          dropSchema: true
-        }),
-        ConfigModule.forRoot(),
-        JwtModule.registerAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: (cso: ConfigService) => {
-            return {
-              secret: cso.get<string>('JWT_KEY'),
-              signOptions: { expiresIn: 60 }
-            }
-          }
-        })
-      ]
-    }).compile()
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [AuthController],
+			providers: [AuthService],
+			imports: [
+				TypeOrmModule.forFeature([UserRepository]),
+				TypeOrmModule.forRoot({
+					type: 'sqlite',
+					database: 'sqlite/1dcc22fc-6dd8-41b8-88a0-39a119c129bf',
+					entities: [UserEntity],
+					synchronize: true,
+					dropSchema: true
+				}),
+				ConfigModule.forRoot(),
+				JwtModule.registerAsync({
+					imports: [ConfigModule],
+					inject: [ConfigService],
+					useFactory: (cso: ConfigService) => {
+						return {
+							secret: cso.get<string>('JWT_KEY'),
+							signOptions: { expiresIn: 60 }
+						}
+					}
+				})
+			]
+		}).compile()
 
-    controller = module.get<AuthController>(AuthController)
-    service = module.get<AuthService>(AuthService)
-  })
+		controller = module.get<AuthController>(AuthController)
+		service = module.get<AuthService>(AuthService)
+	})
 
-  afterEach(async () => {
-    await getConnection().close()
-  })
+	afterEach(async () => {
+		await getConnection().close()
+	})
 
-  describe('login', () => {
+	describe('login', () => {
+		it('creates a jwt token', async () => {
+			jest.spyOn(service, 'validateCredentials').mockImplementation(() =>
+				Promise.resolve({
+					token: undefined,
+					refreshToken: undefined,
+					userId: '6577a9bd-877d-466e-aef5-158a0b8f7bd1'
+				})
+			)
 
-    it('creates a jwt token', async () => {
-      jest.spyOn(service, 'validateCredentials').mockImplementation(() => Promise.resolve({
-        token: undefined,
-        refreshToken: undefined,
-        userId: '6577a9bd-877d-466e-aef5-158a0b8f7bd1'
-      }))
+			let response = await controller.login({
+				email: undefined,
+				password: undefined
+			})
 
-      let response = await controller.login({
-        email: undefined,
-        password: undefined
-      })
+			expect(response.token).toBeDefined()
+			expect(response.token.length).toBeGreaterThan(0)
+		})
 
-      expect(response.token).toBeDefined()
-      expect(response.token.length).toBeGreaterThan(0)
-    })
+		it('returns refresh token and user id', async () => {
+			let refreshToken = '2c20e0ba-0de7-434d-a620-92b6168d8b19'
+			let userId = '1003ff3f-b1ab-4760-9de0-7d65064385d7'
 
-    it('returns refresh token and user id', async () => {
-      let refreshToken = '2c20e0ba-0de7-434d-a620-92b6168d8b19'
-      let userId = '1003ff3f-b1ab-4760-9de0-7d65064385d7'
+			jest.spyOn(service, 'validateCredentials').mockImplementation(() =>
+				Promise.resolve({
+					token: undefined,
+					refreshToken: refreshToken,
+					userId: userId
+				})
+			)
 
-      jest.spyOn(service, 'validateCredentials').mockImplementation(() => Promise.resolve({
-        token: undefined,
-        refreshToken: refreshToken,
-        userId: userId
-      }))
+			let response = await controller.login({
+				email: undefined,
+				password: undefined
+			})
 
-      let response = await controller.login({
-        email: undefined,
-        password: undefined
-      })
+			expect(response.refreshToken).toEqual(refreshToken)
+			expect(response.userId).toEqual(userId)
+		})
+	})
 
-      expect(response.refreshToken).toEqual(refreshToken)
-      expect(response.userId).toEqual(userId)
-    })
+	describe('refresh', () => {
+		it('creates a jwt token', async () => {
+			jest.spyOn(service, 'validateAccessKey').mockImplementation(() => Promise.resolve(true))
 
-  })
+			let response = await controller.refresh({
+				userId: undefined,
+				refreshToken: undefined
+			})
 
-  describe('refresh', () => {
+			expect(response.token).toBeDefined()
+			expect(response.token.length).toBeGreaterThan(0)
+		})
+	})
 
-    it('creates a jwt token', async () => {
-      jest.spyOn(service, 'validateAccessKey').mockImplementation(() => Promise.resolve(true))
+	describe('logout', () => {
+		it('throws an exception when another user logout', async () => {
+			jest.spyOn(service, 'resetAccessKey').mockImplementation()
 
-      let response = await controller.refresh({
-        userId: undefined,
-        refreshToken: undefined
-      })
+			const logoutPromise = async (): Promise<LogoutResponse> =>
+				await controller.logout(
+					{
+						userId: '37d3d432-db0c-42dc-a74e-86c276c3ebfd'
+					},
+					{
+						user: {
+							id: '8d257305-d385-4e16-9252-bfb59e4f2c64'
+						}
+					}
+				)
 
-      expect(response.token).toBeDefined()
-      expect(response.token.length).toBeGreaterThan(0)
-    })
-
-  })
-
-  describe('logout', () => {
-
-    it('throws an exception when another user logout', async () => {
-      jest.spyOn(service, 'resetAccessKey').mockImplementation()
-
-      const logoutPromise = async (): Promise<LogoutResponse> => await controller.logout(
-        {
-          userId: '37d3d432-db0c-42dc-a74e-86c276c3ebfd'
-        },
-        {
-          user: {
-            id: '8d257305-d385-4e16-9252-bfb59e4f2c64'
-          }
-        }
-      )
-
-      await expect(logoutPromise()).rejects.toThrow(ForbiddenException)
-    })
-
-  })
+			await expect(logoutPromise()).rejects.toThrow(ForbiddenException)
+		})
+	})
 })
