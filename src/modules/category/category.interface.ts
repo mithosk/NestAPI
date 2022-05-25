d40a576f232e441e710f375dccd96c1e4592163f
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:07:14 +0200

    Update category.interface.ts

--- src/modules/category/category.interface.ts
+++ src/modules/category/category.interface.ts
@@ -1,13 +1,13 @@
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
 export class CategoryModel {
-    id: string
+	id: string
 
-    @IsString()
-    @IsNotEmpty()
-    code: string
+	@IsString()
+	@IsNotEmpty()
+	code: string
 
-    @IsString()
-    @IsOptional()
-    description: string
+	@IsString()
+	@IsOptional()
+	description: string
 }
