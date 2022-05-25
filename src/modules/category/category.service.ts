6f0d5d9e62b5dcfe2f24928f792162af9a032631
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:07:39 +0200

    Update category.service.ts

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -4,39 +4,34 @@ import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/commo
 
 @Injectable()
 export class CategoryService {
-    constructor(
-        private readonly categoryRepository: CategoryRepository
-    ) { }
+	constructor(private readonly categoryRepository: CategoryRepository) {}
 
-    public async read(id: string): Promise<CategoryModel> {
-        let entity = await this.categoryRepository.findByUuid(id)
-        if (entity === undefined)
-            throw new NotFoundException('category not found')
+	public async read(id: string): Promise<CategoryModel> {
+		let entity = await this.categoryRepository.findByUuid(id)
+		if (entity === undefined) throw new NotFoundException('category not found')
 
-        return {
-            id: entity.uuid,
-            code: entity.code,
-            description: entity.description
-        }
-    }
+		return {
+			id: entity.uuid,
+			code: entity.code,
+			description: entity.description
+		}
+	}
 
-    public async update(category: CategoryModel): Promise<CategoryModel> {
-        let entity = await this.categoryRepository.findByCode(category.code)
-        if (entity !== undefined && entity.uuid !== category.id)
-            throw new ForbiddenException('category code already used')
+	public async update(category: CategoryModel): Promise<CategoryModel> {
+		let entity = await this.categoryRepository.findByCode(category.code)
+		if (entity !== undefined && entity.uuid !== category.id) throw new ForbiddenException('category code already used')
 
-        entity = await this.categoryRepository.findByUuid(category.id)
-        if (entity === undefined)
-            throw new NotFoundException('category not found')
+		entity = await this.categoryRepository.findByUuid(category.id)
+		if (entity === undefined) throw new NotFoundException('category not found')
 
-        entity.code = category.code
-        entity.description = category.description
-        await this.categoryRepository.save(entity)
+		entity.code = category.code
+		entity.description = category.description
+		await this.categoryRepository.save(entity)
 
-        return {
-            id: entity.uuid,
-            code: entity.code,
-            description: entity.description
-        }
-    }
+		return {
+			id: entity.uuid,
+			code: entity.code,
+			description: entity.description
+		}
+	}
 }
