70c07d34979c9f051785a8e9885539e0c192190f
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:02:23 +0200

    Update product.repository.ts

--- src/data/repositories/product.repository.ts
+++ src/data/repositories/product.repository.ts
@@ -6,63 +6,57 @@ import { EntityRepository, Repository, SelectQueryBuilder } from 'typeorm'
 
 @EntityRepository(ProductEntity)
 export class ProductRepository extends Repository<ProductEntity> {
-    public async findByCode(code: string): Promise<ProductEntity> {
-        return await this.createQueryBuilder('pro')
-            .where('pro.code=:code', { code: code })
-            .getOne()
-    }
+	public async findByCode(code: string): Promise<ProductEntity> {
+		return await this.createQueryBuilder('pro').where('pro.code=:code', { code: code }).getOne()
+	}
 
-    public async findByFilter(filter: ProductFilter, sort: ProductSort, skip: number, take: number): Promise<ProductEntity[]> {
-        let query = this.createQueryBuilder('pro')
-            .leftJoinAndSelect('pro.category', 'cat')
+	public async findByFilter(filter: ProductFilter, sort: ProductSort, skip: number, take: number): Promise<ProductEntity[]> {
+		let query = this.createQueryBuilder('pro').leftJoinAndSelect('pro.category', 'cat')
 
-        query = this.applyFilter(query, filter)
+		query = this.applyFilter(query, filter)
 
-        if (sort !== undefined)
-            switch (sort) {
-                case ProductSort.CodeAsc:
-                    query = query.orderBy('pro.code', 'ASC')
-                    break
+		if (sort !== undefined)
+			switch (sort) {
+				case ProductSort.CodeAsc:
+					query = query.orderBy('pro.code', 'ASC')
+					break
 
-                case ProductSort.CodeDesc:
-                    query = query.orderBy('pro.code', 'DESC')
-                    break
+				case ProductSort.CodeDesc:
+					query = query.orderBy('pro.code', 'DESC')
+					break
 
-                case ProductSort.PriceAsc:
-                    query = query.orderBy('pro.price', 'ASC')
-                    break
+				case ProductSort.PriceAsc:
+					query = query.orderBy('pro.price', 'ASC')
+					break
 
-                case ProductSort.PriceDesc:
-                    query = query.orderBy('pro.price', 'DESC')
-                    break
+				case ProductSort.PriceDesc:
+					query = query.orderBy('pro.price', 'DESC')
+					break
 
-                default:
-                    throw new InternalServerErrorException('unmanaged sorting')
-            }
+				default:
+					throw new InternalServerErrorException('unmanaged sorting')
+			}
 
-        if (skip !== undefined)
-            query = query.skip(skip)
+		if (skip !== undefined) query = query.skip(skip)
 
-        if (take !== undefined)
-            query = query.take(take)
+		if (take !== undefined) query = query.take(take)
 
-        return await query.getMany();
-    }
+		return await query.getMany()
+	}
 
-    public async countByFilter(filter: ProductFilter): Promise<number> {
-        let query = this.createQueryBuilder('pro')
+	public async countByFilter(filter: ProductFilter): Promise<number> {
+		let query = this.createQueryBuilder('pro')
 
-        query = this.applyFilter(query, filter)
+		query = this.applyFilter(query, filter)
 
-        return await query.getCount();
-    }
+		return await query.getCount()
+	}
 
-    private applyFilter(query: SelectQueryBuilder<ProductEntity>, filter: ProductFilter) {
-        if (filter.text !== undefined) {
-            query = query
-                .where('pro.code LIKE :text OR pro.description LIKE :text', { text: '%' + filter.text + '%' })
-        }
+	private applyFilter(query: SelectQueryBuilder<ProductEntity>, filter: ProductFilter) {
+		if (filter.text !== undefined) {
+			query = query.where('pro.code LIKE :text OR pro.description LIKE :text', { text: '%' + filter.text + '%' })
+		}
 
-        return query
-    }
+		return query
+	}
 }
