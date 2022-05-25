95907379a22fef8fadfbf7a31cbe109b853d3cd5
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:03:07 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,16 @@ import { ValidationPipe } from '@nestjs/common'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule)
 
-  const config = new DocumentBuilder()
-    .setTitle('Blog API')
-    .setDescription('Blog API')
-    .setVersion('1.0')
-    .build();
+	const config = new DocumentBuilder().setTitle('Blog API').setDescription('Blog API').setVersion('1.0').build()
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, document);
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('/', app, document)
 
-  app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(3000);
+	await app.listen(3000)
 }
 
 bootstrap()
