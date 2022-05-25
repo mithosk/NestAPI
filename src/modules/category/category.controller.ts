4d7276b9a50ea7950c51ef8f906e34a004d89ede
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:07:07 +0200

    Update category.controller.ts

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -5,33 +5,27 @@ import { Body, Controller, ForbiddenException, Param, Patch, UseGuards } from '@
 
 @Controller('categories')
 export class CategoryController {
-    constructor(
-        private readonly service: CategoryService
-    ) { }
+	constructor(private readonly service: CategoryService) {}
 
-    @Patch(':id')
-    @UseGuards(AuthGuard('jwt'))
-    public async patch(@Param('id') id: string, @Body() body: Partial<CategoryModel>): Promise<CategoryModel> {
-        let category = await this.service.read(id)
+	@Patch(':id')
+	@UseGuards(AuthGuard('jwt'))
+	public async patch(@Param('id') id: string, @Body() body: Partial<CategoryModel>): Promise<CategoryModel> {
+		let category = await this.service.read(id)
 
-        //code
-        if (body.code !== undefined) {
-            if (body.code === null)
-                throw new ForbiddenException('code cannot be null')
-            else if (typeof body.code !== 'string')
-                throw new ForbiddenException('code must be a string')
-            else
-                category.code = body.code
-        }
+		//code
+		if (body.code !== undefined) {
+			if (body.code === null) throw new ForbiddenException('code cannot be null')
+			else if (typeof body.code !== 'string') throw new ForbiddenException('code must be a string')
+			else category.code = body.code
+		}
 
-        //description
-        if (body.description !== undefined) {
-            if (body.description !== null && typeof body.description !== 'string')
-                throw new ForbiddenException('description must be a string')
-            else
-                category.description = body.description
-        }
+		//description
+		if (body.description !== undefined) {
+			if (body.description !== null && typeof body.description !== 'string')
+				throw new ForbiddenException('description must be a string')
+			else category.description = body.description
+		}
 
-        return await this.service.update(category)
-    }
+		return await this.service.update(category)
+	}
 }
