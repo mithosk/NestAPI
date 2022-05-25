9ec20f76d5cc8cc05f623619c0e4bf0412fa40a6
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-25 14:08:08 +0200

    Update product.controller.ts

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -6,36 +6,38 @@ import { Body, Controller, Get, Post, Query, Response, Headers, UsePipes, Valida
 
 @Controller('products')
 export class ProductController {
-    constructor(
-        private readonly service: ProductService
-    ) { }
+	constructor(private readonly service: ProductService) {}
 
-    @Post()
-    @UseGuards(AuthGuard('jwt'))
-    public async post(@Body() body: ProductModel): Promise<ProductModel> {
-        return await this.service.create(body)
-    }
+	@Post()
+	@UseGuards(AuthGuard('jwt'))
+	public async post(@Body() body: ProductModel): Promise<ProductModel> {
+		return await this.service.create(body)
+	}
 
-    @Get()
-    @UsePipes(new ValidationPipe({ transform: true }))
-    public async list(@Query() query: ProductQuery, @Headers() headers: { [header: string]: string }, @Response() response: HttpResponse): Promise<void> {
-        let sortType = ProductSortType[headers['sorttype']]
-        sortType = sortType === undefined ? ProductSortType.CodeAsc : sortType
+	@Get()
+	@UsePipes(new ValidationPipe({ transform: true }))
+	public async list(
+		@Query() query: ProductQuery,
+		@Headers() headers: { [header: string]: string },
+		@Response() response: HttpResponse
+	): Promise<void> {
+		let sortType = ProductSortType[headers['sorttype']]
+		sortType = sortType === undefined ? ProductSortType.CodeAsc : sortType
 
-        let pageIndex = parseInt(headers['pageindex'])
-        pageIndex = isNaN(pageIndex) ? 1 : pageIndex
+		let pageIndex = parseInt(headers['pageindex'])
+		pageIndex = isNaN(pageIndex) ? 1 : pageIndex
 
-        let pageSize = parseInt(headers['pagesize'])
-        pageSize = isNaN(pageSize) ? 30 : pageSize
+		let pageSize = parseInt(headers['pagesize'])
+		pageSize = isNaN(pageSize) ? 30 : pageSize
 
-        let page = await this.service.list(query, sortType, pageIndex, pageSize)
+		let page = await this.service.list(query, sortType, pageIndex, pageSize)
 
-        response
-            .set('SortType', ProductSortType[sortType])
-            .set('PageIndex', pageIndex.toString())
-            .set('PageSize', pageSize.toString())
-            .set('PageCount', page.pageCount.toString())
-            .set('ItemCount', page.productCount.toString())
-            .json(page.products)
-    }
+		response
+			.set('SortType', ProductSortType[sortType])
+			.set('PageIndex', pageIndex.toString())
+			.set('PageSize', pageSize.toString())
+			.set('PageCount', page.pageCount.toString())
+			.set('ItemCount', page.productCount.toString())
+			.json(page.products)
+	}
 }
