c39ea095b238119595cc120b3efe76db59ce8425
Luca Nicolini <luca.nicolini@flowing.it>
2022-05-30 17:10:38 +0200

    socket.io example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from './modules/auth/auth.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { ProductModule } from './modules/product/product.module'
 import { CategoryModule } from './modules/category/category.module'
 
 @Module({
-	imports: [AuthModule, CategoryModule, ProductModule, TypeOrmModule.forRoot()]
+	imports: [
+		AuthModule,
+		CategoryModule,
+		ProductModule,
+		TypeOrmModule.forRoot(),
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '..', 'public')
+		})
+	]
 })
 export class AppModule {}
