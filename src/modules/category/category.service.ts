eadc4702144a5c554885a5656ac979eb4e03bb90
Luca Nicolini <luca.nicolini@flowing.it>
2022-07-06 14:56:17 +0200

    Update category.service.ts

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -4,11 +4,14 @@ import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/commo
 
 @Injectable()
 export class CategoryService {
-	constructor(private readonly categoryRepository: CategoryRepository) {}
+	constructor(
+		private readonly categoryRepository: CategoryRepository
+	) { }
 
 	public async read(id: string): Promise<CategoryModel> {
 		const entity = await this.categoryRepository.findByUuid(id)
-		if (entity === undefined) throw new NotFoundException('category not found')
+		if (entity === undefined)
+			throw new NotFoundException('category not found')
 
 		return {
 			id: entity.uuid,
@@ -18,20 +21,22 @@ export class CategoryService {
 	}
 
 	public async update(category: CategoryModel): Promise<CategoryModel> {
-		let entity = await this.categoryRepository.findByCode(category.code)
-		if (entity !== undefined && entity.uuid !== category.id) throw new ForbiddenException('category code already used')
+		let entityByUuid = await this.categoryRepository.findByUuid(category.id)
+		if (entityByUuid === undefined)
+			throw new NotFoundException('category not found')
 
-		entity = await this.categoryRepository.findByUuid(category.id)
-		if (entity === undefined) throw new NotFoundException('category not found')
+		let entityByCode = await this.categoryRepository.findByCode(category.code)
+		if (entityByCode !== undefined && entityByCode.uuid !== entityByUuid.uuid)
+			throw new ForbiddenException('category code already used')
 
-		entity.code = category.code
-		entity.description = category.description
-		await this.categoryRepository.save(entity)
+		entityByUuid.code = category.code
+		entityByUuid.description = category.description
+		await this.categoryRepository.save(entityByUuid)
 
 		return {
-			id: entity.uuid,
-			code: entity.code,
-			description: entity.description
+			id: entityByUuid.uuid,
+			code: entityByUuid.code,
+			description: entityByUuid.description
 		}
 	}
 }
