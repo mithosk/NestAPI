e3cf7114646c850de8de208e532e83fddf3810f3
Luca Nicolini <luca.nicolini@flowing.it>
2022-07-08 09:41:22 +0200

    Update auth.strategy.ts

--- src/modules/auth/auth.strategy.ts
+++ src/modules/auth/auth.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { PassportStrategy } from '@nestjs/passport'
-import { HttpUser } from 'src/definitions/http-user.definition'
+import { HttpUser } from '../../definitions/http-user.definition'
 
 @Injectable()
 export class AuthStrategy extends PassportStrategy(Strategy, 'jwt') {
