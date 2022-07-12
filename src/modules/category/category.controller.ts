d636d53f428eb0e846a91e5b9da3bb57120b36da
Luca Nicolini <luca.nicolini@flowing.it>
2022-07-12 13:19:27 +0200

    Update category.controller.ts

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,11 +1,11 @@
 import { AuthGuard } from '@nestjs/passport'
 import { CategoryModel } from './category.interface'
 import { CategoryService } from './category.service'
-import { Body, Controller, ForbiddenException, Param, Patch, UseGuards } from '@nestjs/common'
+import { BadRequestException, Body, Controller, Param, Patch, UseGuards } from '@nestjs/common'
 
 @Controller('categories')
 export class CategoryController {
-	constructor(private readonly service: CategoryService) {}
+	constructor(private readonly service: CategoryService) { }
 
 	@Patch(':id')
 	@UseGuards(AuthGuard('jwt'))
@@ -14,15 +14,15 @@ export class CategoryController {
 
 		//code
 		if (body.code !== undefined) {
-			if (body.code === null) throw new ForbiddenException('code cannot be null')
-			else if (typeof body.code !== 'string') throw new ForbiddenException('code must be a string')
+			if (body.code === null) throw new BadRequestException('code cannot be null')
+			else if (typeof body.code !== 'string') throw new BadRequestException('code must be a string')
 			else category.code = body.code
 		}
 
 		//description
 		if (body.description !== undefined) {
 			if (body.description !== null && typeof body.description !== 'string')
-				throw new ForbiddenException('description must be a string')
+				throw new BadRequestException('description must be a string')
 			else category.description = body.description
 		}
 
