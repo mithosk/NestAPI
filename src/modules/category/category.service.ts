ce08d86d90b1dcda46f75a2f90bf4270d524c9e1
Luca Nicolini <luca.nicolini@flowing.it>
2022-07-12 10:40:50 +0200

    Update category.service.ts

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -5,22 +5,18 @@ import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/commo
 
 @Injectable()
 export class CategoryService {
-	constructor(
-		private readonly categoryRepository: CategoryRepository
-	) { }
+	constructor(private readonly categoryRepository: CategoryRepository) {}
 
 	public async read(id: string): Promise<CategoryModel> {
 		const entity = await this.categoryRepository.findByUuid(id)
-		if (entity === undefined)
-			throw new NotFoundException('category not found')
+		if (entity === undefined) throw new NotFoundException('category not found')
 
 		return this.map(entity)
 	}
 
 	public async update(category: CategoryModel): Promise<CategoryModel> {
 		const entityByUuid = await this.categoryRepository.findByUuid(category.id)
-		if (entityByUuid === undefined)
-			throw new NotFoundException('category not found')
+		if (entityByUuid === undefined) throw new NotFoundException('category not found')
 
 		const entityByCode = await this.categoryRepository.findByCode(category.code)
 		if (entityByCode !== undefined && entityByCode.uuid !== entityByUuid.uuid)
