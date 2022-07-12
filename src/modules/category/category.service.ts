4750e51cf56271097affaff0533358b3900298cd
Luca Nicolini <luca.nicolini@flowing.it>
2022-07-12 10:40:24 +0200

    Update category.service.ts

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,25 +1,26 @@
 import { CategoryModel } from './category.interface'
+import { CategoryEntity } from '../../data/entities/category.entity'
 import { CategoryRepository } from '../../data/repositories/category.repository'
 import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common'
 
 @Injectable()
 export class CategoryService {
-	constructor(private readonly categoryRepository: CategoryRepository) {}
+	constructor(
+		private readonly categoryRepository: CategoryRepository
+	) { }
 
 	public async read(id: string): Promise<CategoryModel> {
 		const entity = await this.categoryRepository.findByUuid(id)
-		if (entity === undefined) throw new NotFoundException('category not found')
+		if (entity === undefined)
+			throw new NotFoundException('category not found')
 
-		return {
-			id: entity.uuid,
-			code: entity.code,
-			description: entity.description
-		}
+		return this.map(entity)
 	}
 
 	public async update(category: CategoryModel): Promise<CategoryModel> {
 		const entityByUuid = await this.categoryRepository.findByUuid(category.id)
-		if (entityByUuid === undefined) throw new NotFoundException('category not found')
+		if (entityByUuid === undefined)
+			throw new NotFoundException('category not found')
 
 		const entityByCode = await this.categoryRepository.findByCode(category.code)
 		if (entityByCode !== undefined && entityByCode.uuid !== entityByUuid.uuid)
@@ -29,10 +30,14 @@ export class CategoryService {
 		entityByUuid.description = category.description
 		await this.categoryRepository.save(entityByUuid)
 
+		return this.map(entityByUuid)
+	}
+
+	private map(entity: CategoryEntity): CategoryModel {
 		return {
-			id: entityByUuid.uuid,
-			code: entityByUuid.code,
-			description: entityByUuid.description
+			id: entity.uuid,
+			code: entity.code,
+			description: entity.description === null ? undefined : entity.description
 		}
 	}
 }
